21fc1616ba6611981006b5d09c0bac3eb9779bb9
forgedhallpass <13679401+forgedhallpass@users.noreply.github.com>
2022-01-19 12:53:00 +0200

    Added template generation context menu in BurpSuite
    * generate template from an HTTP response, using the selected text in a word matcher
    * add status code matcher based on the HTTP response code

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -25,10 +25,68 @@
 
 package burp;
 
+import io.projectdiscovery.nuclei.gui.TemplateGeneratorWindow;
+import io.projectdiscovery.nuclei.model.*;
+import io.projectdiscovery.nuclei.util.Utils;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.List;
+
 public class BurpExtender implements burp.IBurpExtender {
 
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
         callbacks.setExtensionName("Nuclei");
+
+        callbacks.registerContextMenuFactory(createContextMenuFactory(callbacks));
+    }
+
+    private IContextMenuFactory createContextMenuFactory(IBurpExtenderCallbacks callbacks) {
+        return (IContextMenuInvocation invocation) -> {
+            List<JMenuItem> menuItems = null;
+
+            final byte invocationContext = invocation.getInvocationContext();
+            if (invocationContext == IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_RESPONSE || invocationContext == IContextMenuInvocation.CONTEXT_MESSAGE_VIEWER_RESPONSE) {
+                menuItems = List.of(messageEditorContextMenu(callbacks, invocation));
+            }
+
+            return menuItems;
+        };
+    }
+
+    private JMenuItem messageEditorContextMenu(IBurpExtenderCallbacks iBurpExtenderCallbacks, IContextMenuInvocation invocation) {
+        final JMenuItem menuItem = new JMenuItem("Generate template");
+
+        menuItem.addActionListener((ActionEvent e) -> {
+            final IHttpRequestResponse[] selectedMessages = invocation.getSelectedMessages();
+            if (selectedMessages.length != 0) {
+                generateTemplate(iBurpExtenderCallbacks, selectedMessages[0], invocation.getSelectionBounds());
+            }
+        });
+
+        return menuItem;
+    }
+
+    private void generateTemplate(IBurpExtenderCallbacks callbacks, IHttpRequestResponse requestResponse, int[] selectionBounds) {
+        final byte[] responseBytes = requestResponse.getResponse();
+        final byte[] requestBytes = requestResponse.getRequest();
+
+        final IExtensionHelpers helpers = callbacks.getHelpers();
+
+        final Info info = new Info("Template Name", "forgedhallpass", Info.Severity.info);
+        final Matcher wordMatcher = Utils.createWordMatcher(responseBytes, selectionBounds);
+        final int statusCode = helpers.analyzeResponse(responseBytes).getStatusCode();
+
+        final Requests requests = new Requests();
+        requests.setRaw(requestBytes);
+        requests.setMatchers(wordMatcher, new Status(statusCode));
+
+        final Template template = new Template("template-id", info, requests);
+        final String yamlTemplate = Utils.dumpYaml(template);
+
+        final URL targetUrl = helpers.analyzeRequest(requestResponse.getHttpService(), requestBytes).getUrl();
+        SwingUtilities.invokeLater(() -> new TemplateGeneratorWindow(targetUrl, yamlTemplate, callbacks));
     }
 }
